620b690e371dc3456075ecac2a1683da4d93047c
1057435 <oiercao@gmail.com>
2023-12-04 12:46:26 +0100

    Eskaera

--- model/Eskaera.py
+++ model/Eskaera.py
@@ -1,4 +1,14 @@
 class Eskaera:
-    EID1 = 0
-    EID2 0 __
-    def --new--
+    def __new__(cls, *args, **kwargs):
+        EID1 = args[1]
+        EID2 = args[2]
+        onartua = False
+
+    def getLagunID(self,nireID):
+        if (nireID == self.EID1):
+            return self.EID2
+        else:
+            return self.EID1
+
+    def onartu(self):
+        self.onartua = True
