56f2ef6d505b431ac4e4577155fffb1cbc0a39ba
lsi <lsi@U113942.(none)>
2023-12-04 12:51:22 +0100

    Gai katalogo klasea,gaia taula DB eta LibraryController.py lan egiten

--- controller/LibraryController.py
+++ controller/LibraryController.py
@@ -48,3 +48,27 @@ class LibraryController:
 			return User(user[0][0], user[0][1], user[0][2])
 		else:
 			return None
+'''
+	def search_gaiak(self, title="", limit=6, page=0):
+		count = db.select("""
+				SELECT count() 
+				FROM GAIA G, 
+				WHERE b.author=a.id 
+					AND b.title LIKE ? 
+					AND a.name LIKE ? 
+		""", (f"%{title}%", f"%{author}%"))[0][0]
+		res = db.select("""
+				SELECT b.* 
+				FROM Book b, Author a 
+				WHERE b.author=a.id 
+					AND b.title LIKE ? 
+					AND a.name LIKE ? 
+				LIMIT ? OFFSET ?
+		""", (f"%{title}%", f"%{author}%", limit, limit*page))
+		books = [
+			Book(b[0],b[1],b[2],b[3],b[4])
+			for b in res
+		]
+		return books, count
+		
+'''
