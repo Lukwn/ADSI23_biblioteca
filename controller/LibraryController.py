0a18d7cc267f4bb86e0d0413071a3c3bf0a948f9
OierCao <oiercao@gmail.com>
2023-12-28 19:46:00 +0100

    Oier et Luken Merge

--- controller/LibraryController.py
+++ controller/LibraryController.py
@@ -35,22 +35,6 @@ class LibraryController:
         ]
         return books, count
 
-    def get_user(self, email, password):
-        user = db.select("SELECT * from User WHERE email = ? AND password = ?", (email, hash_password(password)))
-        if len(user) > 0:
-            return User(user[0][0], user[0][1], user[0][2])
-        else:
-            return None
-
-    def get_user_cookies(self, token, time):
-        user = db.select(
-            "SELECT u.* from User u, Session s WHERE u.id = s.user_id AND s.last_login = ? AND s.session_hash = ?",
-            (time, token))
-        if len(user) > 0:
-            return User(user[0][0], user[0][1], user[0][2])
-        else:
-            return None
-
     def get_book(self, id):
         b = db.select("""
                         SELECT Book.*
@@ -60,6 +44,67 @@ class LibraryController:
         tupla = b[0]
         book = Book(tupla[0],tupla[1],tupla[2],tupla[3],tupla[4])
         return book
+
+    def search_lagunak(self, id=0, limit=10, page=0):
+        count = db.select("""
+                    SELECT count()
+                    FROM Eskaera
+                    WHERE (EID1=? OR EID2=?) AND baieztatuta=1
+              """, (id, id))[0][0]
+        res = db.select("""
+                    SELECT *
+                    FROM Eskaera
+                    WHERE (EID1=? OR EID2=?) AND baieztatuta=1
+                    LIMIT ? OFFSET ?
+              """, (id, id, limit, limit * page))
+        lagunak = [
+            self.getUser(e[0]) if e[0] != id else self.getUser(e[1])
+            for e in res
+        ]
+        pictures = [
+            self.get_picture(pictureId.picture)
+            for pictureId in lagunak
+        ]
+        return lagunak, pictures, count
+
+    def getUser(self, id):
+        username = db.select("SELECT * FROM User WHERE id = ?", (id,))
+        if username:
+            return User(username[0][0], username[0][1], username[0][2], username[0][4], username[0][5], username[0][6],
+                        username[0][7])
+        else:
+            return None
+
+    def getUserUsername(self, username):
+        username = db.select("SELECT * FROM User WHERE username = ?", (username,))
+        if username:
+            return User(username[0][0], username[0][1], username[0][2], username[0][4], username[0][5], username[0][6],
+                        username[0][7])
+        else:
+            return None
+
+    def get_user(self, email, password):
+        user = db.select("SELECT * from User WHERE email = ? AND password = ?", (email, hash_password(password)))
+        if len(user) > 0:
+            return User(user[0][0], user[0][1], user[0][2], user[0][4], user[0][5], user[0][6], user[0][7])
+        else:
+            return None
+
+    def get_user_cookies(self, token, time):
+        user = db.select(
+            "SELECT u.* from User u, Session s WHERE u.id = s.user_id AND s.last_login = ? AND s.session_hash = ?",
+            (time, token))
+        if len(user) > 0:
+            return User(user[0][0], user[0][1], user[0][2], user[0][4], user[0][5], user[0][6], user[0][7])
+        else:
+            return None
+
+    def get_picture(self, id):
+        picture = db.select("SELECT link FROM Pictures WHERE ID = ?", (id,))
+        if picture:
+            return picture[0][0]
+        else:
+            return None
 '''
 	def search_gaiak(self, title="", limit=6, page=0):
 		count = db.select("""
